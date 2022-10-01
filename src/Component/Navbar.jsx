c55a3cae4e1165e6f66bd1709fbb8dbc23f95715
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-10-01 08:24:21 +0530

    day-4 work done

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -44,6 +44,7 @@ export default function Navbar() {
           </Link>
         </Flex>
       </Flex>
+      <hr />
     </>
   );
 }
---
+++ src/Component/compare_Working_Data.jsx
@@ -0,0 +1,50 @@
+export const compare_working_data = [
+  {
+    logo1:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/5a4dff992fce4f4e3bf12c3b4f1d3de1",
+    name1: "DXC Techno...",
+    rating1: "3.2",
+    reviews1: "3.8K reviews",
+    logo2:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/ce43c82fedc38bf5739f8c90376c3b98",
+    name2: "Cognizant",
+    rating2: "3.9",
+    reviews2: "15K reviews",
+  },
+  {
+    logo1:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/bb45fcbde221cc5789506c52752573de",
+    name1: "Nokia",
+    rating1: "4.1",
+    reviews1: "5.1K reviews",
+    logo2:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/c50f14536e0deeae1493678dfe91b816",
+    name2: "Ericsson...",
+    rating2: "4.1",
+    reviews2: "6.7K reviews",
+  },
+  {
+    logo1:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/acfc069b6351c9d83b5b58e07ccb6f49",
+    name1: "Maersk",
+    rating1: "4.1",
+    reviews1: "2.8K reviews",
+    logo2:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/b383e18c353ed5ebd98c4632d593caed",
+    name2: "CMA CGM",
+    rating2: "3.6",
+    reviews2: "189 reviews",
+  },
+  {
+    logo1:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/d0e5d561c303274719c2e37f3ce69ca8",
+    name1: "Jio",
+    rating1: "3.9",
+    reviews1: "1.5K reviews",
+    logo2:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/64x64/057ebdac02c3a6964e062df70a91f2b6",
+    name2: "Reliance Jio..",
+    rating2: "4.0",
+    reviews2: "102 reviews",
+  },
+];
